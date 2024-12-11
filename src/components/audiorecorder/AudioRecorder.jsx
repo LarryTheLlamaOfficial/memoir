1dba591e412f041ab0dbafb526dedd878a54a4d9
Matthew Hackett <123338547+LarryTheLlamaOfficial@users.noreply.github.com>
2024-12-12 10:19:23 +1100

    Now mp3

--- src/components/audiorecorder/AudioRecorder.jsx
+++ src/components/audiorecorder/AudioRecorder.jsx
@@ -1,6 +1,6 @@
 import { useState, useRef } from "react";
 
-const mimeType = "audio/webm";
+const mimeType = "audio/mp3";
 
 function AudioRecorder() {
     const [permission, setPermission] = useState(false);

d78907e438375fb1e4c3b341f352a4e8d2b1d503
shminge <77525090+shminge@users.noreply.github.com>
2024-12-17 11:32:32 +1100

    Added login protection to record

--- src/pages/record/Record.jsx
+++ src/pages/record/Record.jsx
@@ -3,13 +3,16 @@ import Header from "../../components/header/Header.jsx";
 
 import AudioRecorder from "../../components/audiorecorder/AudioRecorder.jsx";
 
+import { auth } from "../../config/firebase.js";
+
 function Record() {
     return (
-        <>
-            <Header />
-            <AudioRecorder/>
-        </>
-    );
+    <>
+        < Header />
+        {auth?.currentUser?.uid ? < AudioRecorder /> : <></>}
+    </>
+    )
+    
 }
 
 export default Record;

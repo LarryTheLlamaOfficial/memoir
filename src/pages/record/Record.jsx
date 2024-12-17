ce3a35080f1bd1bcc284c6c9fbe40f4d5c5bf5f7
shminge <77525090+shminge@users.noreply.github.com>
2024-12-17 11:21:45 +1100

    Added Record page

---
+++ src/pages/record/Record.jsx
@@ -0,0 +1,15 @@
+
+import Header from "../../components/header/Header.jsx";
+
+import AudioRecorder from "../../components/audiorecorder/AudioRecorder.jsx";
+
+function Record() {
+    return (
+        <>
+            <Header />
+            <AudioRecorder/>
+        </>
+    );
+}
+
+export default Record;

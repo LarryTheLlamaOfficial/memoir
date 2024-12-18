f26ffb4d6f486be3f0a077792e84db3b6292fae4
Matthew Hackett <123338547+LarryTheLlamaOfficial@users.noreply.github.com>
2024-12-18 18:36:57 +1100

    Fixed secret key

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -7,8 +7,11 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-import {onRequest} from "firebase-functions/v2/https";
+//import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+
+import * as functions from "firebase-functions";
+//import axios from "axios";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
@@ -18,6 +21,37 @@ import * as logger from "firebase-functions/logger";
 //   response.send("Hello from Firebase!");
 // });
 
-export const createTranscript = onRequest(() => {
+export const createTranscript = functions.storage.onObjectFinalized({
+    secrets: ["DEEPGRAM_KEY"]
+},
+    async (object) => {
+        const fileBucket = object.data.bucket; // Storage bucket containing the file.
+        const filePath = object.data.name; // File path in the bucket.
+        const contentType = object.data.contentType;
 
-});
+        if (fileBucket != 'memoir-1296.firebasestorage.app') {
+            logger.log("Accessing from wrong bucket")
+            return;
+        }
+
+        if (contentType != "audio/mpeg") {
+            logger.log("Not an audio file")
+            return;
+        }
+        
+        const split = filePath.split("/")
+
+        if (split.length != 4) {
+            logger.log("Must be length 4");
+            return;
+        }
+
+        if (split[0] == "users" && split[2] == "audio") {
+            const uid = split[1];
+            const rawFileName = split[4];
+            console.log(uid + rawFileName); // To remove unused errors
+        } else {
+
+        }
+    }
+);

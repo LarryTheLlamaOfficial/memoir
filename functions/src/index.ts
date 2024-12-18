4e02fb146524c2b65ab87572ddb3c70706a7e8f3
Matthew Hackett <123338547+LarryTheLlamaOfficial@users.noreply.github.com>
2024-12-18 15:22:06 +1100

    Functions so far

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,3 +17,7 @@ import * as logger from "firebase-functions/logger";
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+export const createTranscript = onRequest(() => {
+
+});

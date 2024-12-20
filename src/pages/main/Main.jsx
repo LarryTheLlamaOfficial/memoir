6d29280008837ab48e0d472aa3c27e343ff91a4d
Matthew Hackett <123338547+LarryTheLlamaOfficial@users.noreply.github.com>
2024-12-20 21:33:17 +1100

    Added content panel

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,11 +1,34 @@
 import Footer from "../../components/footer/Footer";
 import styles from "./Main.module.css";
+import { useState } from "react";
 
 function Main() {
+    const contentPanel = <div className={styles.panel}></div>;
+
+    const recordPanel = null;
+
+    const accountPanel = null;
+
+    var [panel, setPanel] = useState("content");
+
+    const contentSwitch = (name) => {
+        switch (name) {
+            case "content":
+                return contentPanel;
+            case "record":
+                return recordPanel;
+            case "account":
+                return accountPanel;
+        }
+    };
+
     return (
         <div className={styles.main}>
-            <div id='contents'></div>
-            <Footer />
+            <div id='contents'>{contentSwitch(panel)}</div>
+            <div className={styles.footer}>
+                <Footer />
+            </div>
+            
         </div>
     );
 }
